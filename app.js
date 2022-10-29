a4e2140268105f081ec38a06b9db0d280cd57266
Abdulahad4464 <abdulahadabdurahmanov25@gmail.com>
2022-10-29 11:21:19 +0500

    ex-exam

---
+++ app.js
@@ -0,0 +1,11 @@
+const home = document.querySelector(".home")
+const about = document.querySelector(".about")
+
+
+function showHome(){
+    home.classList.toggle("show_home")
+}
+
+function showabout(){
+    about.classList.toggle("show_about")
+}
